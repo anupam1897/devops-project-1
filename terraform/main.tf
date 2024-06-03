b7687c5e6652d9b78e18fe59cd5b5a43e4a8d2f1
anupam1897 <anupam1897@gmail.com>
2024-06-03 10:34:47 +0530

    ec2 profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "group1" {
 }
 
 resource "aws_iam_instance_profile" "ec2-profile" {
-  name = "ec2-profile-${random_string.suffix.result}"
+  name = "ec2-profile-1"
   role = "ec2-ecr-auth"
 }
 
